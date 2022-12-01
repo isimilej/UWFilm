2a047d1622799b9956299bfd1c75c2bef3f81fe3
Kwangjin <isimilej@gmail.com>
2022-12-01 15:18:46 +0900

    feat: 영화 포스터 Glide 사용 표시

--- app/src/main/java/com/android/play/uwfilm/DetailFragment.kt
+++ app/src/main/java/com/android/play/uwfilm/DetailFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.android.play.uwfilm.data.movie.Movies
 import com.android.play.uwfilm.databinding.FragmentDetailBinding
+import com.bumptech.glide.Glide
 import kotlinx.coroutines.launch
 
 class DetailFragment(val movieCode: String) : Fragment() {
@@ -24,6 +25,8 @@ class DetailFragment(val movieCode: String) : Fragment() {
         lifecycleScope.launch {
             var content = Movies().fetchMovieInformation(movieCode)
             Log.e("DETAIL", "CONTENT=$content")
+
+            Glide.with(this@DetailFragment).load(content).into(binding.thumb)
         }
         return binding.root
     }
